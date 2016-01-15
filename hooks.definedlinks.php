ec2252dd7d4ba18d68ce19bd63ea6bca4d58e292
highpanda <sambarnett@highpanda.com>
2016-01-15 13:31:06 +0000

    Update hooks.definedlinks.php

--- hooks.definedlinks.php
+++ hooks.definedlinks.php
@@ -1,14 +1,37 @@
 <?php
 
-	class Hooks_definedlinks extends Hooks
-	{
-		public function control_panel__add_to_head()
-		{
-			if (URL::getCurrent(false) == '/publish')
-			{
-				return $this->js->link('definedlinks.js');
-			}
-		}
-	}
+    class Hooks_definedlinks extends Hooks
+    {
+        public function control_panel__add_to_head()
+        {
+            if (URL::getCurrent(false) == '/publish')
+            {
+                $file = 'defined-links.json';
+                $current_content_list = file_get_contents($file, true);
+                $current_content_list = json_decode($current_content_list);
+
+                $content_set = ContentService::getContentByFolders(array("/*"));
+                $content_set->multisort('url');
+                $content_set = $content_set->get();
+
+                $content_list = [];
+
+                foreach ($content_set as $content){
+                    if (isset($content['url']) && isset($content['title'])){
+                        $name = '[' . $content['url'] . '] ' . $content['title'];
+                        $url = $content['url'];
+                        $content_list[] = [ 'name' => $name, 'url' => $url ];
+                    }
+                }
+
+                if ($current_content_list !== $content_list){
+                    $json = json_encode($content_list);
+                    file_put_contents($file,$json, FILE_USE_INCLUDE_PATH);
+                }
+
+                return $this->js->link('definedlinks.js');
+            }
+        }
+    }
 
 ?>
